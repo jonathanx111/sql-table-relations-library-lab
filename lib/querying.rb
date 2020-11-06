dc863b3828ac4666a2d60924165c6721b465a5c3
jonathanx111 <jonathanx111@gmail.com>
2020-11-05 23:15:23 -0500

    final

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,45 +1,40 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books
-  WHERE series_id = 1
-  ORDER BY books.year;"
+"SELECT books.title, books.year FROM books
+WHERE books.series_id = 1;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto FROM characters
-  ORDER BY LENGTH(motto) DESC
+  "SELECT characters.name, characters.motto FROM characters
+  ORDER BY LENGTH(characters.motto) DESC
   LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, count(species) FROM characters
-  GROUP BY species
-  ORDER BY count(species) DESC
-  LIMIT 1;"
+"SELECT characters.species, COUNT(characters.species) FROM characters
+GROUP BY characters.species ORDER BY COUNT(characters.species) DESC
+LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors
-  INNER JOIN series ON authors.id = series.author_id
-  INNER join subgenres ON series.subgenre_id = subgenres.id;"
+ "SELECT authors.name, subgenres.name FROM authors
+ JOIN series ON authors.id = series.author_id
+ JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series
-  INNER JOIN authors ON series.author_id = authors.id
-  INNER JOIN characters ON authors.id = characters.author_id
-  WHERE characters.species = 'human'
-  GROUP BY series.title
-  ORDER BY count(species = 'human') DESC
-  LIMIT 1
-  ;"
+"SELECT series.title FROM series
+JOIN authors ON series.author_id = authors.id
+JOIN characters ON authors.id = characters.author_id
+GROUP BY characters.species
+HAVING characters.species = 'human'
+ORDER BY COUNT(characters.species) DESC
+LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(*) as book_count
-FROM character_books
-    JOIN characters
-    ON character_books.character_id = characters.id
+"SELECT characters.name, COUNT(characters.name) AS num FROM characters
+JOIN character_books ON characters.id = character_books.character_id
 GROUP BY characters.name
-ORDER BY book_count DESC; "
+ORDER BY num DESC;"
 end
